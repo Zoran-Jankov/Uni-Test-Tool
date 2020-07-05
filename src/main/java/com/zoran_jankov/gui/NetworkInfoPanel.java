c225b9005eb788ee97143e0dc0fc164e412e6c73
Zoran Jankov <zoran.jankov.87@gmail.com>
2020-07-05 15:02:04 +0200

    NetworkInfoPanel

---
+++ src/main/java/com/zoran_jankov/gui/NetworkInfoPanel.java
@@ -0,0 +1,16 @@
+package main.java.com.zoran_jankov.gui;
+
+import javax.swing.JPanel;
+import net.miginfocom.swing.MigLayout;
+
+public class NetworkInfoPanel extends JPanel
+{
+	/**
+	 * Create the panel.
+	 */
+	public NetworkInfoPanel()
+	{
+		setLayout(new MigLayout("", "[]", "[]"));
+		
+	}
+}
