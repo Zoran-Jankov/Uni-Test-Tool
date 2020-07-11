894b7063a5848bcedccbaf7e1e56ec7ac5e2672a
Zoran Jankov <zoran.jankov.87@gmail.com>
2020-07-11 11:36:01 +0200

    Create HardDiskModel.java

---
+++ src/main/java/com/zoran_jankov/model/HardDiskModel.java
@@ -0,0 +1,68 @@
+package main.java.com.zoran_jankov.model;
+
+import java.io.File;
+
+public class HardDiskModel
+{
+	private static final int KB = 1024;
+	private static final int MB = 1048576;
+	private static final int GB = 1073741824;
+	
+	public long getTotalSpace(String partitionName)
+	{
+		File partition = new File(partitionName);
+		
+		long freeSpace = partition.getTotalSpace();
+		
+		return freeSpace;
+	}
+	
+	public long getFreeSpace(String partitionName)
+	{
+		
+		File partition = new File(partitionName);
+		
+		long freeSpace = partition.getFreeSpace();
+		
+		return freeSpace;
+	}
+	
+	public long getUsedSpace(String partitionName)
+	{
+		File partition = new File(partitionName);
+		
+		long freeSpace = partition.getTotalSpace() - partition.getFreeSpace();
+		
+		return freeSpace;
+	}
+	
+	public long getFreeSpacePercentage(String partitionName)
+	{
+		return getFreeSpace(partitionName) / getTotalSpace(partitionName) * 100;
+	}
+	
+	public static String toSting(long diskSpace)
+	{
+		
+		String diskSpaceInfo;
+		
+		if(diskSpace / GB > 0)
+		{
+			diskSpaceInfo = String.valueOf(diskSpace / GB) + " GB";
+		}
+		else if(diskSpace / MB > 0)
+		{
+			diskSpaceInfo = String.valueOf(diskSpace / MB) + " MB";
+		}
+		else if(diskSpace / KB > 0)
+		{
+			diskSpaceInfo = String.valueOf(diskSpace / KB) + " kB";
+		}
+		else
+		{
+			diskSpaceInfo = String.valueOf(diskSpace) + " B";
+		}
+		
+		return diskSpaceInfo;
+	}
+}
