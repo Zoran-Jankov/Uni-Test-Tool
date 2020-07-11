d919391abff145537e9a478b1e13c3ef7d19bb2a
Zoran Jankov <zoran.jankov.87@gmail.com>
2020-07-11 15:02:22 +0200

    HardDiskInfo update

--- src/main/java/com/zoran_jankov/controller/HardDiskPanelController.java
+++ src/main/java/com/zoran_jankov/controller/HardDiskPanelController.java
@@ -5,16 +5,25 @@ import main.java.com.zoran_jankov.model.HardDiskUtils;
 
 public class HardDiskPanelController
 {
+	private static final String PARTITION_NAME = "C:";
+	
 	private HardDiskPanel hardDiskPanel;
+	
 	
 	HardDiskPanelController(HardDiskPanel hardDiskPanel)
 	{
 		this.hardDiskPanel = hardDiskPanel;
-		setCPartitionUsage();
+		setCPartition();
 	}
 	
-	public void setCPartitionUsage()
+	public void setCPartition()
 	{
-		hardDiskPanel.setCPartitionUsage(HardDiskUtils.getUsedSpacePercentage("C:"));
+		hardDiskPanel.setCPartitionUsage(HardDiskUtils.getUsedSpacePercentage(PARTITION_NAME));
+		
+		String partitionInfo = HardDiskUtils.toSting(HardDiskUtils.getFreeSpace(PARTITION_NAME))
+							 + " free of "
+							 + HardDiskUtils.toSting(HardDiskUtils.getTotalSpace(PARTITION_NAME));
+		
+		hardDiskPanel.setPartitionInfo(partitionInfo);
 	}
 }
--- src/main/java/com/zoran_jankov/gui/HardDiskPanel.java
+++ src/main/java/com/zoran_jankov/gui/HardDiskPanel.java
@@ -3,25 +3,42 @@ package main.java.com.zoran_jankov.gui;
 import javax.swing.JPanel;
 import net.miginfocom.swing.MigLayout;
 import javax.swing.JProgressBar;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.SwingConstants;
 
 public class HardDiskPanel extends JPanel
 {
 	private static final long serialVersionUID = 2722800090244927075L;
 	
 	private JProgressBar cPartitionBar = new JProgressBar();
+	private JLabel lblPartitionInfo = new JLabel();
 
 	/**
 	 * Create the panel.
 	 */
 	public HardDiskPanel()
 	{
-		setLayout(new MigLayout("", "[150px:n]", "[50px:n]"));
+		setLayout(new MigLayout("", "[300px:n]", "[][30px:n][]"));
 		
-		add(cPartitionBar, "cell 0 0,grow");
+		JLabel lblLocalDiskC = new JLabel("Local Disk (C:)");
+		lblLocalDiskC.setFont(new Font("Tahoma", Font.BOLD, 14));
+		add(lblLocalDiskC, "cell 0 0");
+		
+		add(cPartitionBar, "cell 0 1,grow");
+		lblPartitionInfo.setHorizontalAlignment(SwingConstants.CENTER);
+		
+		
+		add(lblPartitionInfo, "cell 0 2");
 	}
 	
 	public void setCPartitionUsage(int partitionUsage)
 	{
 		cPartitionBar.setValue(partitionUsage);
+	}
+	
+	public void setPartitionInfo(String info)
+	{
+		lblPartitionInfo.setText(info);
 	}
 }
